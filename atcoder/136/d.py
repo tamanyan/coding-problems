805d67ff7f89b3499b4554b7c950962ec4f3b5f3
Taketo Yoshida <tamanyan.sss@gmail.com>
2019-08-23 00:39:04 +0700

    Update 136 d.py

--- atcoder/136/d.py
+++ atcoder/136/d.py
@@ -1,40 +1,49 @@
-import heapq
+maps = [s for s in str(input())]
+people = [{'pos': i, 'prev': -1, 'loop': -1} for i in range(len(maps))]
+max_count = 10**100
 
 
-class Job:
-
-    def __repr__(self):
-        return "{0},{1}".format(self.days, self.reward)
-
-    def __lt__(self, other):
-        return self.reward < other.reward
-
-    def __init__(self, job_id, reward, days):
-        self.id = job_id
-        self.reward = reward
-        self.days = days
-
-N, M = list(map(int, input().split()))
-h = []
-jobs = []
-
-for i in range(N):
-    A, B = list(map(int, input().split()))
-    jobs.append(Job(job_id=i, reward=B, days=A))
+def update(people, index):
+    for p in people:
+        if maps[p['pos']] == 'R':
+            if p['prev'] == p['pos'] + 1:
+                p['loop'] = index
+            p['prev'] = p['pos']
+            p['pos'] += 1
+        else:
+            if p['prev'] == p['pos'] - 1:
+                p['loop'] = index
+            p['prev'] = p['pos']
+            p['pos'] -= 1
 
 
-jobs = sorted(jobs, key=lambda j: j.days)
-idx = 0
-c = 0
+def get_output(people):
+    output = [0] * len(people)
 
-for i in range(1, M+1):
-    while idx < N and jobs[idx].days <= i:
-        heapq.heappush(h, -jobs[idx].reward)
-        idx += 1
+    for p in people:
+        output[p['pos']] += 1
 
-    # print(i)
-    if len(h) != 0:
-        # print(h)
-        c += -heapq.heappop(h)
+    return ' '.join(str(i) for i in output)
 
-print(c)
+
+for index in range(max_count):
+    # print(index, 'before', get_output(people))
+    update(people, index)
+    # print(index, 'after', get_output(people))
+
+    if len([p for p in people if p['loop'] < 0]) == 0:
+        # print(get_output(people))
+        for p in people:
+            mod = (max_count - p['loop'] + 1) % 2
+            # print(mod)
+            for i in range(mod):
+                if maps[p['pos']] == 'R':
+                    p['prev'] = p['pos']
+                    p['pos'] += 1
+                else:
+                    p['prev'] = p['pos']
+                    p['pos'] -= 1
+        # print(get_output(people))
+        break
+
+print(get_output(people))
