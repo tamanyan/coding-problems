99d80f8fa529711845291acbd49b2a2fb9b9d106
Taketo Yoshida <tamanyan.sss@gmail.com>
2019-08-27 23:46:57 +0700

    Solving 135 d

--- atcoder/135/d.py
+++ atcoder/135/d.py
@@ -1,14 +1,19 @@
-from collections import Counter
+def rec(S, k):
+    if k == len(S):
+        return 1
+
+    ret = 0
+
+    if S[k] == '?':
+        for i in range(10):
+            ret += rec(S, k+1)
+    else:
+        ret += rec(S, k+1)
+
+    return ret
+
 
 S = input()
 MOD = 10**9 + 7
 
-c = Counter(S)
-n = c['?']
-
-for i in range(2 ** n):
-    total = 0
-    print("pattern {}: ".format(i), end="")
-    for j in range(n):
-        if ((i >> j) & 1):
-            print(j)
+print(rec(S, 0))
