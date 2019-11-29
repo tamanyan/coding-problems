3ddb80763a97db984b046e724c8c4075dedf224e
Taketo Yoshida <tamanyan.sss@gmail.com>
2019-11-29 22:08:42 +0700

    Add ABC A/B

---
+++ ABC/114/a.py
@@ -0,0 +1,62 @@
+from heapq import heappush, heappop, heapify
+from collections import deque, defaultdict, Counter
+import itertools
+from itertools import permutations, combinations
+import sys
+import bisect
+import string
+import math
+import time
+
+input = sys.stdin.readline
+
+# input = sys.stdin.readline
+sys.setrecursionlimit(2147483647)
+
+
+def I(): return int(input())
+
+
+def MI(): return map(int, input().split())
+
+
+def LI(): return [int(i) for i in input().split()]
+
+
+def LI_(): return [int(i)-1 for i in input().split()]
+
+
+def StoI(): return [ord(i)-97 for i in input()]
+
+
+def ItoS(nn): return chr(nn+97)
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+def show(*inp, end='\n'):
+    if show_flg:
+        print(*inp, end=end)
+
+
+YN = ['Yes', 'No']
+MOD = 10**9+7
+inf = float('inf')
+l_alp = string.ascii_lowercase
+u_alp = string.ascii_uppercase
+ts = time.time()
+sys.setrecursionlimit(10**6)
+
+
+def main():
+    X = int(input())
+
+    if X == 7 or X == 5 or X == 3:
+        print('YES')
+    else:
+        print('NO')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ABC/114/b.py
@@ -0,0 +1,64 @@
+from heapq import heappush, heappop, heapify
+from collections import deque, defaultdict, Counter
+import itertools
+from itertools import permutations, combinations
+import sys
+import bisect
+import string
+import math
+import time
+
+input = sys.stdin.readline
+
+# input = sys.stdin.readline
+sys.setrecursionlimit(2147483647)
+
+
+def I(): return int(input())
+
+
+def MI(): return map(int, input().split())
+
+
+def LI(): return [int(i) for i in input().split()]
+
+
+def LI_(): return [int(i)-1 for i in input().split()]
+
+
+def StoI(): return [ord(i)-97 for i in input()]
+
+
+def ItoS(nn): return chr(nn+97)
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+def show(*inp, end='\n'):
+    if show_flg:
+        print(*inp, end=end)
+
+
+YN = ['Yes', 'No']
+MOD = 10**9+7
+inf = float('inf')
+iinf = 10**19
+l_alp = string.ascii_lowercase
+u_alp = string.ascii_uppercase
+ts = time.time()
+sys.setrecursionlimit(10**6)
+
+
+def main():
+    S = input()
+    ans = iinf
+
+    for i in range(len(S)-2):
+        ans = min(ans, abs(753 - int(S[i:i+3])))
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
