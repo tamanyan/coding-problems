77f79c1f2c14b35b4999bc350b7b382d5f80078b
Taketo Yoshida <tamanyan.sss@gmail.com>
2020-03-16 00:31:41 +0700

    Add panasonic2020

--- other/panasonic2020/c.py
+++ other/panasonic2020/c.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-import collections
+from decimal import *
 from heapq import heappush, heappop, heapify
 from collections import deque, defaultdict, Counter
 import itertools
@@ -14,10 +13,10 @@ import time
 def I(): return int(input())
 
 
-def MI(): return map(int, input().split())
-
-
 def S(): return input()
+
+
+def MI(): return map(int, input().split())
 
 
 def MS(): return map(str, input().split())
@@ -38,58 +37,46 @@ def ItoS(nn): return chr(nn+97)
 def input(): return sys.stdin.readline().rstrip()
 
 
-def print_matrix(mat):
-    for i in range(len(mat)):
-        print(*mat[i])
-
-
 def show(*inp, end='\n'):
     if show_flg:
         print(*inp, end=end)
 
 
-YN = {False: 'No', True: 'Yes'}
+YNL = {False: 'No', True: 'Yes'}
+YNU = {False: 'NO', True: 'YES'}
 MOD = 10**9+7
 inf = float('inf')
 IINF = 10**10
 l_alp = string.ascii_lowercase
 u_alp = string.ascii_uppercase
+ts = time.time()
+sys.setrecursionlimit(10**6)
 nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
 show_flg = False
 # show_flg = True
 
 
-def solve(T):
-    n, k = MI()
-    a = LI()
-    MAX = 10**16 + 1
-    v = 1
+def int_sqrt2(n):
+    def f(prev):
+        while True:
+            m = (prev + n / prev) / 2
+            if m >= prev:
+                return prev
+            prev = m
 
-    while v < MAX:
-        v *= k
+    return f(int(math.sqrt(n) * (1 + 1e-10)))
 
-    # print(v)
-
-    while v > 0:
-        for i in range(n):
-            if a[i] >= v:
-                a[i] -= v
-                break
-        v //= k
-
-    # print(*a)
-    if all([a[i] == 0 for i in range(n)]):
-        print('YES')
-    else:
-        print('NO')
 
 
 def main():
-    T = I()
+    a, b, c = MI()
+    getcontext().prec = 20
 
-    for i in range(T):
-        solve(T)
+    if Decimal(a).sqrt() + Decimal(b).sqrt() < Decimal(c).sqrt():
+        print('Yes')
+    else:
+        print('No')
 
 
 if __name__ == '__main__':
--- other/panasonic2020/f.py
+++ other/panasonic2020/f.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-import collections
 from heapq import heappush, heappop, heapify
 from collections import deque, defaultdict, Counter
 import itertools
@@ -14,10 +12,10 @@ import time
 def I(): return int(input())
 
 
-def MI(): return map(int, input().split())
-
-
 def S(): return input()
+
+
+def MI(): return map(int, input().split())
 
 
 def MS(): return map(str, input().split())
@@ -38,58 +36,30 @@ def ItoS(nn): return chr(nn+97)
 def input(): return sys.stdin.readline().rstrip()
 
 
-def print_matrix(mat):
-    for i in range(len(mat)):
-        print(*mat[i])
-
-
 def show(*inp, end='\n'):
     if show_flg:
         print(*inp, end=end)
 
 
-YN = {False: 'No', True: 'Yes'}
+YNL = {False: 'No', True: 'Yes'}
+YNU = {False: 'NO', True: 'YES'}
 MOD = 10**9+7
 inf = float('inf')
 IINF = 10**10
 l_alp = string.ascii_lowercase
 u_alp = string.ascii_uppercase
+ts = time.time()
+sys.setrecursionlimit(10**6)
 nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
 show_flg = False
 # show_flg = True
 
 
-def solve(T):
-    n, k = MI()
-    a = LI()
-    MAX = 10**16 + 1
-    v = 1
-
-    while v < MAX:
-        v *= k
-
-    # print(v)
-
-    while v > 0:
-        for i in range(n):
-            if a[i] >= v:
-                a[i] -= v
-                break
-        v //= k
-
-    # print(*a)
-    if all([a[i] == 0 for i in range(n)]):
-        print('YES')
-    else:
-        print('NO')
-
-
 def main():
-    T = I()
+    N = I()
 
-    for i in range(T):
-        solve(T)
+    print()
 
 
 if __name__ == '__main__':
