7b176d6a6fc4c1582fecf523a50f7ef1b522d028
Taketo Yoshida <tamanyan.sss@gmail.com>
2020-03-24 16:40:57 +0700

    Add AGC 015

---
+++ other/code_festival/c.py
@@ -0,0 +1,78 @@
+from heapq import heappush, heappop, heapify
+from collections import deque, defaultdict, Counter
+import itertools
+from itertools import permutations, combinations, accumulate
+import sys
+import bisect
+import string
+import math
+import time
+
+
+def I(): return int(input())
+
+
+def S(): return input()
+
+
+def MI(): return map(int, input().split())
+
+
+def MS(): return map(str, input().split())
+
+
+def LI(): return [int(i) for i in input().split()]
+
+
+def LI_(): return [int(i)-1 for i in input().split()]
+
+
+def StoI(): return [ord(i)-97 for i in input()]
+
+
+def ItoS(nn): return chr(nn+97)
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+def show(*inp, end='\n'):
+    if show_flg:
+        print(*inp, end=end)
+
+
+YNL = {False: 'No', True: 'Yes'}
+YNU = {False: 'NO', True: 'YES'}
+MOD = 10**9+7
+inf = float('inf')
+IINF = 10**10
+l_alp = string.ascii_lowercase
+u_alp = string.ascii_uppercase
+ts = time.time()
+sys.setrecursionlimit(10**6)
+nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+show_flg = False
+# show_flg = True
+
+
+def base_10_to_n(X, n):
+    if int(X / n):
+        return base_10_to_n(int(X/n), n) + str(X % n)
+    return str(X % n)
+
+
+def main():
+    A = I()
+
+    for i in range(10, 10000+1):
+        print(i, base_10_to_n(A, i))
+        if str(i) == base_10_to_n(A, i):
+            print(i)
+            return
+
+    print(-1)
+
+
+if __name__ == '__main__':
+    main()
